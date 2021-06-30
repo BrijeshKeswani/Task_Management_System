6c1856e05c671e3e9d27567dd7d596326f68a430
BrijeshKeswani <2019pietcsbrijesh41@poornima.org>
2021-06-30 11:45:03 +0530

    added contentEditable Attribute

--- index.js
+++ index.js
@@ -6,13 +6,14 @@ const generateNewCard = (taskData) => `
  <div class="col-md-6 col-lg-4" id=${taskData.id}>
  <div class="card text-center">
    <div class="card-header d-flex justify-content-end gap-2">
-     <button type="button" class="btn btn-outline-success"><i class="fas fa-pencil-alt"></i>
+     <button type="button"  id=${taskData.id} class="btn btn-outline-success"  onclick="editCard.apply(this, arguments)">
+     <i class="fas fa-pencil-alt" id=${taskData.id} onclick="editCard.apply(this, arguments)"></i>
      </button>
 <button type="button" class="btn btn-outline-danger" id=${taskData.id} onclick="deleteCard.apply(this, arguments)">
 <i class="fas fa-trash" id=${taskData.id} onclick="deleteCard.apply(this, arguments)"></i>
 </button>
    </div>
-   <img src=${taskData.imageUrl}
+   <img src=${taskData.imageUrl} class = "card-img-top" alt="..."/>
    <div class="card-body">
      <h5 class="card-title">${taskData.taskTitle}</h5>
      <p class="card-text">${taskData.taskDescription}</p>
@@ -102,4 +103,29 @@ const deleteCard = (event) => {
 
 //so here we will access local storage --> with some methods(like add,delete,update etc.) --> using javascript
 
+const editCard =  (event) => {
+  event = window.event;
+  const targetId = event.target.id;
+  const tagname = event.target.tagName;
 
+  let parentElement;
+
+  if (tagname === "BUTTON") {
+    parentElement=event.target.parentNode.parentNode; 
+  }
+  else {
+    parentElement=event.target.parentNode.parentNode.parentNode;
+  }
+
+   
+  let taskTitle = parentElement.childNodes[5].childNodes[1];
+  let taskDescription = parentElement.childNodes[5].childNodes[3];
+  let taskType = parentElement.childNodes[5].childNodes[5];
+  let submitButton = parentElement.childNodes[7].childNodes[1];
+  taskTitle.setAttribute("contenteditable" , "true");
+  taskDescription.setAttribute("contenteditable" , "true");
+  taskType.setAttribute("contenteditable" , "true");
+  submitButton.innerHTML = "Save Changes"; //it will change open task button to save changes during edtitng time
+  
+  
+ } 
